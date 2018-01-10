0388b9ab893371acf48dd91c9fdc3c4789b5cba1
joye <wangxiaolong@joycastle.mobi>
2018-01-10 15:19:10 +0800

    test

--- gameplugin/src/main/java/com/joycastle/gameplugin/NativeUtil.java
+++ gameplugin/src/main/java/com/joycastle/gameplugin/NativeUtil.java
@@ -1,6 +1,11 @@
 package com.joycastle.gameplugin;
 
+import android.util.Log;
+
 import com.joycastle.gamepluginbase.SystemUtil;
+import com.joycastle.my_facebook.FacebookHelper;
+
+import java.lang.reflect.Method;
 
 /**
  * Created by gaoyang on 10/19/16.
@@ -19,14 +24,27 @@ public class NativeUtil {
         _instance = this;
 
         System.out.print("NativeUtil test###");
+
+        Log.e("native test log", "init: ", NativeUtil.nativeTest());
     }
 
     public void Test(){
         System.out.print("its native test function!");
+        try{
+            Class clazz = Class.forName("com.joycastle.social_facebook.FacebookHelper");
+            Method method = clazz.getMethod("getInstance");
+            FacebookHelper fbHelper = (FacebookHelper)method.invoke(null);
+            fbHelper.isLogin();
+
+        }catch (Exception e){
+            System.out.print("method no refrence!");
+        }
+
     }
 
     public static String invokeJavaMethod(String claxx, String method, String data, int requestId) {
         return String.valueOf(SystemUtil.getCpuTime());
     }
 
+    public static native String nativeTest();
 }
