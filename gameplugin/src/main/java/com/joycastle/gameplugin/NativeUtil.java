4c8f234a4930ac2b450fdfb3231ea840ec930d33
joye <wangxiaolong@joycastle.mobi>
2018-01-15 18:00:15 +0800

    updata nativeUtil

--- gameplugin/src/main/java/com/joycastle/gameplugin/NativeUtil.java
+++ gameplugin/src/main/java/com/joycastle/gameplugin/NativeUtil.java
@@ -48,7 +48,8 @@ public class NativeUtil {
     private static String invokeJavaMethod(String className, String methodName, String reqData) {
         String resData = "{}";
         try {
-            JSONArray reqArray = new JSONArray(reqData);
+            JSONObject reqObj = new JSONObject(reqData);
+            JSONArray reqArray = new JSONArray(reqObj.getString("json"));
             Class clazz = Class.forName(className);
             Method getInstanceMethod = clazz.getMethod("getInstance");
             Object instance = getInstanceMethod.invoke(null);
@@ -80,7 +81,8 @@ public class NativeUtil {
     private static String invokeJavaMethodAsync(String className, String methodName, String reqData, InvokeJavaMethodDelegate listener) {
         String resData = "{}";
         try {
-            JSONArray reqArray = new JSONArray(reqData);
+            JSONObject reqObj = new JSONObject(reqData);
+            JSONArray reqArray = new JSONArray(reqObj.getString("json"));
             Class clazz = Class.forName(className);
             Method getInstanceMethod = clazz.getMethod("getInstance");
             Object instance = getInstanceMethod.invoke(null);
