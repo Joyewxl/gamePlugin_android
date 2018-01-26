7d5267349b773e9a657fd4bdcf5b88d83940badc
joye <wangxiaolong@joycastle.mobi>
2018-01-26 14:26:42 +0800

    updata notification

--- gameplugin_base/src/main/java/com/joycastle/gamepluginbase/SystemUtil.java
+++ gameplugin_base/src/main/java/com/joycastle/gamepluginbase/SystemUtil.java
@@ -2,7 +2,11 @@ package com.joycastle.gamepluginbase;
 
 import android.app.Activity;
 import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -212,6 +216,26 @@ public class SystemUtil {
     }
 
     public static void postNotication(ArrayList<Map<String, Object>> notifications) {
+        Log.e(TAG, "postNotication: "+notifications.toString());
+//        Context mContext = application.getApplicationContext();
+//        // 获取通知服务对象NotificationManager
+//        NotificationManager notiManager = (NotificationManager)activity.getSystemService(mContext.NOTIFICATION_SERVICE);
+//        Notification.Builder builder = new Notification.Builder(mContext);
+////        Intent intent = new Intent(context, MainActivity.class);  //需要跳转指定的页面
+////        PendingIntent pendingIntent = PendingIntent.getActivity(mContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+////        builder.setContentIntent(pendingIntent);
+////        builder.setSmallIcon(R.mipmap.ic_launcher);// 设置图标
+//        builder.setContentTitle("标题");// 设置通知的标题
+//        builder.setContentText("内容");// 设置通知的内容
+//        builder.setWhen(System.currentTimeMillis());// 设置通知来到的时间
+//        builder.setAutoCancel(true); //自己维护通知的消失
+//        builder.setTicker("new message");// 第一次提示消失的时候显示在通知栏上的
+//        builder.setOngoing(true);
+//        builder.setNumber(20);
+//
+//        Notification notification = builder.build();
+//        notification.flags = Notification.FLAG_NO_CLEAR;  //只有全部清除时，Notification才会清除
+//        notiManager.notify(0,notification);
 
     }
 
