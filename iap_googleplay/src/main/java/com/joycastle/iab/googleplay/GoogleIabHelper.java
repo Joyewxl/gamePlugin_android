0f9300203af85892af23dcc7e31818385b681ffd
joye <wangxiaolong@joycastle.mobi>
2019-06-17 14:35:53 +0800

    :art:完善内购逻辑判断

--- iap_googleplay/src/main/java/com/joycastle/iab/googleplay/GoogleIabHelper.java
+++ iap_googleplay/src/main/java/com/joycastle/iab/googleplay/GoogleIabHelper.java
@@ -294,6 +294,18 @@ public class GoogleIabHelper implements LifeCycleDelegate, IabBroadcastReceiver.
                     SystemUtil.getInstance().setConsumeHandler(new InvokeJavaMethodDelegate() {
                         @Override
                         public void onFinish(final ArrayList<Object> resArrayList) {
+
+                            if (resArrayList.size() > 0 && resArrayList.get(0) != null) {
+                                HashMap map = (HashMap) resArrayList.get(0);
+                                boolean _isSuccess = (boolean)map.get("isSuccess");
+                                String _environment = (String)map.get("environment");
+
+                                if (_isSuccess == false && _environment == "") {
+                                    //不关闭订单
+                                    return;
+                                }
+                            }
+
                             try {
                                 mHelper.consumeAsync(info, new IabHelper.OnConsumeFinishedListener() {
                                     @Override
